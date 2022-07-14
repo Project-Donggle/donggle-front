f2e8e62b3b2e3397ecff1ef0ef8279a33246bbce
awesomeji <thswl1515@gmail.com>
2022-07-14 17:18:15 +0900

    가져오는 위치값을 스테이트로 못넘기는중

--- src/UI/pages/pageMap.tsx
+++ src/UI/pages/pageMap.tsx
@@ -5,79 +5,92 @@ declare global {
     kakao: any;
   }
 }
-interface LocPosition{
-    La: number;
-    Ma: number;
- }
+//현재 위,경도 담을 타입
+type LocPosition ={
+    La?: number,
+    Ma?: number ;
+}
+ 
+
 function pageMap() {
-
-    const [center, setCenter] = useState({})
-    const [map,setMap] =useState({})
+    //현재위치스테이트
+    const [currentPosition, setCurrentPosition] = useState<LocPosition>({});
+    
+useEffect(() => {
+     //받은 위치와 메세지로 지도와 마커(메세지포함)띄움       
+function displayMarker(locPosition: LocPosition, message: string) {
   
+const container = document.getElementById('map')
+const options : object = {
+    center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+    level: 5,
+    }
 
-    useEffect(() => {
-        
-        
-        
-        function displayMarker(locPosition: LocPosition, message: string) {
-            setCenter(new window.kakao.maps.LatLng(33.450701, 126.570667))
-            console.log(center)
-            const container = document.getElementById('map')
-            const options = {
-                center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-                level: 3,
-            }
-            console.log(typeof new window.kakao.maps.LatLng(33.450701, 126.570667))
-            console.log(new window.kakao.maps.LatLng(33.450701, 126.570667))
-            setMap(new window.kakao.maps.Map(container, options))
-           
-            let map = new window.kakao.maps.Map(container, options); // 지도를 생성합니다
-       
-          // 마커를 생성합니다
-          let marker = new window.kakao.maps.Marker({  
-              map: map, 
-              position: locPosition
-          }); 
-          
-         
+let map = new window.kakao.maps.Map(container, options); 
     
-    let iwContent = message, // 인포윈도우에 표시할 내용
-        iwRemoveable = true;
 
-    // 인포윈도우를 생성합니다
-    let infowindow = new window.kakao.maps.InfoWindow({
-        content : iwContent,
-        removable : iwRemoveable
-    });
+let marker = new window.kakao.maps.Marker({  
+    map: map, 
+    position: locPosition
+}); 
+ 
+
+let iwContent = message, // 인포윈도우에 표시할 내용
+    iwRemoveable = true;
     
-    // 인포윈도우를 마커위에 표시합니다 
-    infowindow.open(map, marker);
+// 인포윈도우를 생성합니다
+let infowindow = new window.kakao.maps.InfoWindow({
+    content : iwContent,
+    removable : iwRemoveable
+});
     
-    // 지도 중심좌표를 접속위치로 변경합니다
-   
+// 인포윈도우를 마커위에 표시합니다 
+infowindow.open(map, marker);
+
+// 지도 중심좌표를 접속위치로 변경합니다
+map.setCenter(locPosition);   
         }    
         
-
-
-      if (navigator.geolocation) { 
-           navigator.geolocation.getCurrentPosition(function(position) {
-        
-        var lat = position.coords.latitude, // 위도
-            lon = position.coords.longitude; // 경도
-        
-        var locPosition = new window.kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-            message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
-        
-        // 마커와 인포윈도우를 표시합니다
-        displayMarker(locPosition, message);
-            
-      });
-      } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
     
-    var locPosition = new window.kakao.maps.LatLng(33.450701, 126.570667),    
-        message = 'geolocation을 사용할수 없어요..'
+//현재위치 api사용가능하면    
+    if (navigator.geolocation) { 
+    
+    const options = {
+    enableHighAccuracy: true,//배터리를 많이소모하더라도 정확한 위치를 알기위해서
+    timeout: 5000,  
+    maximumAge: 0
+        };
+
+    function error() {
+    console.log("error");
+            };
         
+    navigator.geolocation.getCurrentPosition(function(position) {
+    console.log(position)
+    const { latitude, longitude } = position.coords;
+    const lat = latitude; // 위도
+    const lon = longitude; // 경도
+    //왜 안담아지는걸까?    
+    setCurrentPosition({
+        ...currentPosition,
+        La: lat,
+        Ma: lon
+    })
+    console.log(currentPosition)
+
+    let locPosition = new window.kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+        message = '<div style="padding:5px;">현재내위치</div>'; // 인포윈도우에 표시될 내용입니다
+        
+    // 위치정보와 성공메세지를 디스플레이하는 함수에 떨굼
     displayMarker(locPosition, message);
+        
+  },error,options);
+  } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+
+    let locPosition = new window.kakao.maps.LatLng(33.450701, 126.570667),
+    message = '위치정보추적불가';
+    
+displayMarker(locPosition, message);
 }
   }, [])
   
